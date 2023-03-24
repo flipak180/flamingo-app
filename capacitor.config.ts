11fb419fe8096485c647af60fda9a05711221804
flipak180@mail.ru <flipak180@mail.ru>
2023-03-24 15:43:25 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lets-go',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
