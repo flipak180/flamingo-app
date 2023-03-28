94975349603b28447bf71a9c41175c11cd46189a
flipak180@mail.ru <flipak180@mail.ru>
2023-03-28 20:29:21 +0300

    map modal

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -1,35 +1,75 @@
 <template>
-    <ion-page>
+    <ion-page ref="page">
         <ion-header>
             <ion-toolbar>
-                <ion-title>Tab 1</ion-title>
+                <ion-title>Места</ion-title>
+                <ion-buttons slot="primary">
+                    <ion-button id="open-modal">
+                        <ion-icon slot="icon-only" :icon="mapOutline"></ion-icon>
+                    </ion-button>
+                </ion-buttons>
             </ion-toolbar>
         </ion-header>
-        <ion-content :fullscreen="true">
-            <ion-header collapse="condense">
-                <ion-toolbar>
-                    <ion-title size="large">Tab 1</ion-title>
-                </ion-toolbar>
-            </ion-header>
+        <ion-content>
+            <ion-refresher slot="fixed" @ionRefresh="refresh($event)">
+                <ion-refresher-content />
+            </ion-refresher>
+            <div class="ion-margin-top ion-text-center" v-if="isLoading">
+                <ion-spinner />
+            </div>
 
             <div id="container">
                 <strong>{{ store.coords.latitude }}</strong> <br>
                 <strong>{{ store.coords.longitude }}</strong>
             </div>
+
+            <ion-modal ref="modal" trigger="open-modal" :presenting-element="presentingElement">
+                <ion-header>
+                    <ion-toolbar>
+                        <ion-title>Modal</ion-title>
+                        <ion-buttons slot="end">
+                            <ion-button @click="dismiss()">Close</ion-button>
+                        </ion-buttons>
+                    </ion-toolbar>
+                </ion-header>
+                <ion-content class="ion-padding">
+                    <p>123</p>
+                </ion-content>
+            </ion-modal>
         </ion-content>
     </ion-page>
 </template>
 
 <script setup lang="ts">
-import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/vue';
+import {
+    IonButton,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonIcon,
+    IonModal,
+    IonPage,
+    IonRefresher,
+    IonRefresherContent,
+    IonSpinner,
+    IonTitle,
+    IonToolbar
+} from '@ionic/vue';
+import {mapOutline} from 'ionicons/icons';
 import {Geolocation} from '@capacitor/geolocation';
-import {onMounted} from "vue";
+import {onMounted, ref} from "vue";
 import {useMainStore} from "@/store";
 
 const store = useMainStore()
 
+const isLoading = ref(false);
+const presentingElement = ref(null);
+const modal = ref(null)
+const page = ref(null)
+
 onMounted(() => {
     printCurrentPosition();
+    presentingElement.value = page.value.$el;
 })
 
 const printCurrentPosition = async () => {
@@ -41,6 +81,15 @@ const printCurrentPosition = async () => {
     console.log('Current position:', coordinates);
     store.setCoords(coordinates.coords.latitude, coordinates.coords.longitude);
 }
+
+const refresh = (event) => {
+    event.target.complete();
+}
+
+const dismiss = () => {
+    modal.value.$el.dismiss();
+}
+
 </script>
 
 <style scoped>
