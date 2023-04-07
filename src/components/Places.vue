6985084761326610b27f6c587af8ec772c318ab0
flipak180@mail.ru <flipak180@mail.ru>
2023-04-08 01:51:21 +0300

    visits

--- src/components/Places.vue
+++ src/components/Places.vue
@@ -4,7 +4,7 @@
             <ion-card-title>{{ place.title }}</ion-card-title>
         </ion-card-header>
         <ion-card-content>
-            <ion-button size="small" :disabled="!atPlace(place)">Я тут</ion-button>
+            <ion-button size="small" :disabled="!atPlace(place)" @click="visit(place)">Я тут</ion-button>
             <ion-button size="small" @click="openMap(place)">
                 <ion-icon slot="icon-only" :icon="mapOutline"></ion-icon>
             </ion-button>
@@ -13,7 +13,16 @@
 </template>
 
 <script>
-import {IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonIcon} from "@ionic/vue";
+import {
+    IonButton,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonIcon,
+    toastController
+} from "@ionic/vue";
 import {mapOutline} from "ionicons/icons";
 import axios from "axios";
 import {atPlace} from "@/models/Place";
@@ -47,6 +56,18 @@ export default {
         },
         async openMap(place) {
             this.$router.push({ name: 'map', params: { place_id: place.id } });
+        },
+        async visit(place) {
+            await axios.get(`https://flamingo.spb.ru/api/places/visit?place_id=${place.id}`).then(res => {
+
+            });
+
+            const toast = await toastController.create({
+                message: 'Визит сохранен',
+                duration: 1500,
+            });
+
+            await toast.present();
         }
     }
 }
