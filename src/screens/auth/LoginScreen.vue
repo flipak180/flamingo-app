21ae81dee2c7eeb915a26935ce5cfbcd985c191b
nikitabelaev <flipak180@mail.ru>
2023-04-29 22:53:22 +0300

    LoginScreen.vue

--- src/screens/auth/LoginScreen.vue
+++ src/screens/auth/LoginScreen.vue
@@ -7,17 +7,12 @@
         </ion-header>
         <ion-content>
             <ion-grid>
-                <ion-row class="flex-column">
-                    <ion-col style="max-height: 30%;">
-                        <div class="ion-padding ion-text-center">
-                            <img src="/assets/logo.png" width="250" alt="">
-                        </div>
-                    </ion-col>
+                <ion-row>
                     <ion-col>
                         <form @submit.prevent="phoneSubmit">
                             <ion-item class="ion-margin-bottom">
                                 <ion-label position="stacked">Телефон</ion-label>
-                                <ion-input v-model="phone" type="tel" inputmode="tel" placeholder="+7"></ion-input>
+                                <ion-input v-model="phone" type="tel" inputmode="tel" placeholder="+7" aria-label="Телефон"></ion-input>
                                 <input v-mask="'+7 (###) ###-##-##'" v-model="phone" style="display: none">
                             </ion-item>
                             <ion-button expand="block" type="submit" color="primary">Подтвердить</ion-button>
@@ -95,29 +90,11 @@ export default {
 </script>
 
 <style scoped>
-.input-cont {
-    padding: 2px;
+ion-grid {
+    height: 100%;
 }
-.input-cont input {
-    color: #000;
-    background: #fff;
-    border: none;
-    font-size: 21px;
-    padding: 10px 15px;
-    border-radius: 10px;
-    display: block;
-    width: 100%;
-}
-.flex-column {
-    flex-direction: column;
-    height: calc(100vh - 44px);
-}
-.button-links {
-    display: flex;
-    justify-content: space-between;
-}
-.button-links a {
-    color: var(--ion-color-primary);
-    text-decoration: none;
+ion-row {
+    align-items: center;
+    height: 100%;
 }
 </style>
