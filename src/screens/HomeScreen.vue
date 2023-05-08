6e164051dc49d007892925553c160ea9e8a16bf5
flipak180@mail.ru <flipak180@mail.ru>
2023-05-08 15:29:06 +0300

    home screen

--- src/screens/HomeScreen.vue
+++ src/screens/HomeScreen.vue
@@ -20,6 +20,29 @@
             <div class="ion-margin-top ion-text-center" v-if="isLoading">
                 <ion-spinner />
             </div>
+
+            <div class="ion-padding-horizontal">
+                <h2>Актуальное</h2>
+                <div class="collections">
+                    <div class="collection-item">
+                        <div class="collection-item__image" style="background-image: url('https://7oom.ru/powerpoint/fony-dlya-prezentacii-po-istorii-03.jpg')">
+
+                        </div>
+                        <div class="collection-item__title">Никто не забыт, ничто не забыто!</div>
+                    </div>
+                    <div class="collection-item">
+                        <div class="collection-item__image" style="background-image: url('https://avatars.mds.yandex.net/i?id=1fb54feba749cd90e6b320e2cce0eba6dad6183e-8427413-images-thumbs&n=13')">
+
+                        </div>
+                        <div class="collection-item__title">Король и Шут. Между Купчино и Ржевкой</div>
+                    </div>
+                </div>
+
+                <h2>Подборки</h2>
+            </div>
+
+
+
             <CategoriesGrid :categories="categories" />
             <CardModal />
         </ion-content>
@@ -98,6 +121,37 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+h2 {
+    margin: 30px 0 20px;
+}
 
+.collections {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 30px;
+}
+
+.collection-item {
+    border-radius: 10px;
+    background: #fff;
+    box-shadow: 0 5px 10px 2px rgba(var(--black-rgba), 0.2);
+
+    &__image {
+        height: 150px;
+        background-repeat: no-repeat;
+        background-size: cover;
+        //border-top-left-radius: 5px;
+        //border-top-right-radius: 5px;
+
+        box-shadow: 0 5px 5px -5px rgba(34, 60, 80, 0.6);
+        border-radius: 10px;
+    }
+
+    &__title {
+        font-size: 14px;
+        font-weight: 700;
+        padding: 10px;
+    }
+}
 </style>
