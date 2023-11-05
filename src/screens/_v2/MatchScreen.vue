86e2aa755397f7c17e74577384e8d7f3afd85b65
flipak180@mail.ru <flipak180@mail.ru>
2023-11-06 01:59:10 +0300

    swipe cards

--- src/screens/_v2/MatchScreen.vue
+++ src/screens/_v2/MatchScreen.vue
@@ -4,25 +4,94 @@ import {locationOutline, optionsOutline, shuffleOutline} from 'ionicons/icons';
 import {Swiper, SwiperSlide} from "swiper/vue";
 import 'swiper/css';
 import '@ionic/vue/css/ionic-swiper.css';
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
 import {Pagination} from "swiper";
 
-const slides = ref();
+const slides = ref([]);
 const modules = ref([Pagination]);
 const setSwiperInstance = (swiper) => {
-    console.log(swiper);
-    slides.value = swiper;
+    slides.value.push(swiper);
+    console.log(slides.value);
 }
 
-const nextImg = () => {
-    console.log(123);
-    slides.value.slideNext();
-}
+onMounted(() => {
+    var animating = false;
+    var cardsCounter = 0;
+    var numOfCards = 6;
+    var decisionVal = 80;
+    var pullDeltaX = 0;
+    var deg = 0;
+    var $card, $cardReject, $cardLike;
 
-const prevImg = () => {
-    console.log(321);
-    slides.value.slidePrev();
-}
+    function pullChange() {
+        animating = true;
+        deg = pullDeltaX / 10;
+        //$card.css("transform", "translateX("+ pullDeltaX +"px) rotate("+ deg +"deg)");
+        $card.css("transform", "translateX("+ pullDeltaX +"px)");
+
+        var opacity = pullDeltaX / 100;
+        var rejectOpacity = (opacity >= 0) ? 0 : Math.abs(opacity);
+        var likeOpacity = (opacity <= 0) ? 0 : opacity;
+        $cardReject.css("opacity", rejectOpacity);
+        $cardLike.css("opacity", likeOpacity);
+    }
+
+    function release() {
+
+        if (pullDeltaX >= decisionVal) {
+            $card.addClass("to-right");
+        } else if (pullDeltaX <= -decisionVal) {
+            $card.addClass("to-left");
+        }
+
+        if (Math.abs(pullDeltaX) >= decisionVal) {
+            $card.addClass("inactive");
+
+            setTimeout(function() {
+                $card.addClass("below").removeClass("inactive to-left to-right");
+                cardsCounter++;
+                if (cardsCounter === numOfCards) {
+                    cardsCounter = 0;
+                    $(".swipe-card").removeClass("below");
+                }
+            }, 300);
+        }
+
+        if (Math.abs(pullDeltaX) < decisionVal) {
+            $card.addClass("reset");
+        }
+
+        setTimeout(function() {
+            $card.attr("style", "").removeClass("reset")
+                .find(".swipe-card__choice").attr("style", "");
+
+            pullDeltaX = 0;
+            animating = false;
+        }, 300);
+    }
+
+    $(document).on("mousedown touchstart", ".swipe-card:not(.inactive)", function(e) {
+        if (animating) return;
+
+        $card = $(this);
+        $cardReject = $(".swipe-card__choice.m--reject", $card);
+        $cardLike = $(".swipe-card__choice.m--like", $card);
+        var startX =  e.pageX || e.originalEvent.touches[0].pageX;
+
+        $(document).on("mousemove touchmove", function(e) {
+            var x = e.pageX || e.originalEvent.touches[0].pageX;
+            pullDeltaX = (x - startX);
+            if (!pullDeltaX) return;
+            pullChange();
+        });
+
+        $(document).on("mouseup touchend", function() {
+            $(document).off("mousemove touchmove mouseup touchend");
+            if (!pullDeltaX) return; // prevents from rapid click events
+            release();
+        });
+    });
+})
 </script>
 
 <template>
@@ -43,7 +112,7 @@ const prevImg = () => {
     </ion-header>
     <ion-content>
         <div class="swipe-cards">
-            <div class="swipe-card">
+            <div class="swipe-card" v-for="index in 5" :key="index">
                 <div class="swipe-card__top">
                     <swiper class="swipe-card__slider" @swiper="setSwiperInstance" :pagination="true" :modules="modules">
                         <swiper-slide class="swipe-card__slider-item" :style="{ backgroundImage: `url(${require('@/assets/swipe-card-1.jpg')})` }"></swiper-slide>
@@ -54,17 +123,19 @@ const prevImg = () => {
                         <div class="swipe-card__tag">Выставка</div>
                     </div>
                     <div class="swipe-card__img-controls">
-                        <div class="swipe-card__prev-img" @click="prevImg"></div>
-                        <div class="swipe-card__next-img" @click="nextImg"></div>
+                        <div class="swipe-card__prev-img" @click="slides[index - 1].slidePrev()"></div>
+                        <div class="swipe-card__next-img" @click="slides[index - 1].slideNext()"></div>
                     </div>
                 </div>
                 <div class="swipe-card__info">
-                    <div class="swipe-card__title">Эрарта</div>
+                    <div class="swipe-card__title">Эрарта {{ index }}</div>
                     <div class="swipe-card__distance">
                         <ion-icon :icon="locationOutline"></ion-icon>
                         15 км от вас
                     </div>
                 </div>
+                <div class="swipe-card__choice m--reject"></div>
+                <div class="swipe-card__choice m--like"></div>
             </div>
         </div>
     </ion-content>
@@ -102,9 +173,16 @@ ion-content {
 }
 .swipe-card {
     background: #232323;
-    height: 100%;
     max-height: 650px;
     border-radius: 15px;
+
+    z-index: 2;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    transform-origin: 50% 100%;
 
     &__top {
         position: relative;
@@ -167,6 +245,82 @@ ion-content {
         display: flex;
         align-items: center;
         gap: 5px;
+    }
+
+
+    &.reset {
+        transition: transform 0.3s;
+        transform: translateX(0) !important;
+
+        .demo__card__choice {
+            transition: opacity 0.3s;
+            opacity: 0 !important;;
+        }
+    }
+
+    &.inactive {
+        transition: transform 0.3s;
+    }
+
+    &.to-left {
+        //transform: translateX(-30rem) rotate(-30deg) !important;
+        transform: translateX(-30rem) !important;
+    }
+
+    &.to-right {
+        //transform: translate(30rem) rotate(30deg) !important;
+        transform: translate(30rem) !important;
+    }
+
+    &.below {
+        z-index: 1;
+    }
+
+    &__choice {
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        opacity: 0;
+        border-radius: 15px;
+
+        &:before {
+            content: "";
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            width: 2rem;
+            height: 2rem;
+            margin-left: -1rem;
+            color: #fff;
+            border-radius: 50%;
+            box-shadow: -2rem -3rem #fff, 2rem -3rem #fff;
+        }
+
+        &:after {
+            content: "";
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            width: 4rem;
+            height: 1.5rem;
+            margin-left: -2rem;
+            border: 0.6rem solid #fff;
+            border-bottom: none;
+            border-radius: 15px;
+        }
+
+        &.m--reject {
+            background: #ff6262;
+        }
+        &.m--like {
+            background: #B1DA96;
+
+            &:after {
+                transform: scaleY(-1);
+            }
+        }
     }
 }
 </style>
