ad86f30c55f2ff58c5a0f0b89846ff38a82360b1
nikitabelaev <flipak180@mail.ru>
2023-11-29 19:26:00 +0300

    fix

--- src/components/CloseButton.vue
+++ src/components/CloseButton.vue
@@ -44,7 +44,7 @@ export default {
     border-radius: 50%;
     font-size: 20px;
     transition: all .3s;
-    z-index: 2;
+    z-index: 50;
 
     @media (prefers-color-scheme: dark) {
         background: rgba(0, 0, 0, 0.8);
