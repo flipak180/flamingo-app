f895895ad5b98a67970b75df9e8abdfaf3484478
skavorodka <flipak180@mail.ru>
2024-01-08 01:45:40 +0300

    places

--- src/screens/_v2/CategoryScreen.vue
+++ src/screens/_v2/CategoryScreen.vue
@@ -2,11 +2,14 @@
     <ion-page>
         <ion-header>
             <ion-toolbar>
+                <ion-buttons slot="start">
+                    <BackButton />
+                </ion-buttons>
                 <ion-title>Flamin<span class="highlighted">GO</span></ion-title>
             </ion-toolbar>
         </ion-header>
         <ion-content class="ion-padding">
-
+            <PlacesList :places="places" />
         </ion-content>
     </ion-page>
 </template>
@@ -30,10 +33,12 @@ import CardModal from "@/components/CardModal.vue";
 import CardsList from "@/components/_v2/ArticlesList.vue";
 import api from "@/plugins/api";
 import CategoryItem from "@/components/_v2/CategoryItem.vue";
+import PlacesList from "@/components/_v2/PlacesList.vue";
 
 export default {
     name: "HomeScreen",
     components: {
+        PlacesList,
         CategoryItem,
         IonRefresher, CardsList, IonRefresherContent, IonSpinner, CardModal,
         IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
